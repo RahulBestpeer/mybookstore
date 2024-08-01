0b937d3ac7719f7502ee6fdc4130342ebf3a719f
RahulBestpeer <rapatel@bestpeers.com>
2024-08-01 18:12:29 +0530

    adding some code

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,7 @@ class CartsController < ApplicationController
   load_and_authorize_resource only: %i[create destroy]
 
   def create
-    if !Cart.find_by(book_id: params[:book_id]).nil?
+    if !(Cart.find_by(book_id: params[:book_id]) and Cart.find_by(account_id:current_account.id)).nil?
       flash[:message] = 'book already added to your cart'
       redirect_to controller: 'books', action: 'index'
     else
